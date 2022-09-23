9bba310e39e8240798ed722ac7ca0bcaf0520507
arun prajapati <arunkp1122@gmail.com>
2022-09-23 20:25:41 +0545

    Added swagger and user api urls

--- server/CannBann/CannBann/urls.py
+++ server/CannBann/CannBann/urls.py
@@ -1,17 +1,44 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
+
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenVerifyView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from userprofile.views import UserProfileViewSet
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 router = DefaultRouter()
+router.register(r'user',UserProfileViewSet)
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/',include(router.urls)),
     path(r'admin/', admin.site.urls),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
